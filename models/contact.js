7ff9ba86e186ace0f0088abe7089fc764caeec4e
Necip Arg <necip.arg@zingat.com>
2016-08-23 13:13:24 +0300

    Implement contact model and CRUDL

---
+++ models/contact.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * @since 21/08/16
+ * @author Necip Arg <neciparg@gmail.com>
+ */
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Contact = mongoose.model('Contact', {
+    owner: Schema.Types.Mixed,
+    name: Schema.Types.Mixed,
+    gender: String,
+    location: Schema.Types.Mixed,
+    email: String,
+    phone: String,
+    cell: String,
+    picture: Schema.Types.Mixed,
+    created: Number,
+    updated: Number
+});
+
+module.exports = Contact;
